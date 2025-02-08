73674401e0ebcb233b4df371a919e423af917d98
onobrain <dreemerups@gmail.com>
2025-02-08 16:37:50 +0100

    vercel resolve 2

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
+};
+
+module.exports = nextConfig;
